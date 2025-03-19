5e2e94e04ae20ef427fef520a75b2acb579854ea
Matias Orellana <meorellanaramirez@gmail.com>
2025-03-19 16:21:40 -0300

    Render config

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import io
 
 # Configuración de la aplicación
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super_clave_secreta_tengosueño'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/votacion.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'super_clave_secreta_tengosueño')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database/votacion.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializar la base de datos
@@ -353,4 +353,5 @@ def initialize_database():
 
 # Iniciar la aplicación
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
